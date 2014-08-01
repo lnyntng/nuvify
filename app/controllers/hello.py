68d41c7b6b77a5a5a8572a8263f741045a7cee4d
katy <lnyntng@gmail.com>
2014-08-01 16:05:25 -0600

    call list.html from home page

--- app/controllers/hello.py
+++
@@ -1,7 +0,0 @@
-from ferris import Controller
-
-
-class Hello(Controller):
-
-    def list(self):
-        return "Hello, is it me you're looking for?"
--- nuvify.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module("nuvify", [])
-	.controller("SongController", function($scope) {
-        $scope.songs = {};
-        $scope.songs.count = 1;
-    } );
