918dc9c055da239074be602f42767c4e6aa39d18
Isaac Martinez <Isaac.Martinez@laureate.net>
2014-08-01 16:27:27 -0600

    esto no funciona

--- nuvify.js
+++ nuvify.js
@@ -3,3 +3,11 @@ var app = angular.module("nuvify", [])
         $scope.songs = {};
         $scope.songs.count = 1;
     } );
+
+   app.controller('TagsCtrl', function($scope, $http) {
+  	$http.get('http://localhost:8080/api/tags')
+       .then(function(res){
+          $scope.tags = res.data; 
+          Console.log(res.data);               
+        });
+});
