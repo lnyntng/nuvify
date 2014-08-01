d6d35bdf03cc15eab718dc827ba9e8c1acd62dc9
Isaac Martinez <Isaac.Martinez@laureate.net>
2014-08-01 15:37:40 -0600

    added routs and stuff to the controllers

--- app/controllers/Albums.py
+++ app/controllers/Albums.py
@@ -2,11 +2,20 @@ from ferris import Controller, scaffold, messages
 
 class Albums(Controller):
 	class Meta:
-		prefixes = ('api',)
-		components = (scaffold.Scaffolding, messages.Messaging)
+		prefixes = ('api', 'admin',)
+		components = (scaffold.Scaffolding, messages.Messaging,)
 
+	add = scaffold.add
+	list = scaffold.list
+	view = scaffold.view
+	edit = scaffold.edit	
 	api_list = scaffold.list
 	api_view = scaffold.view
 	api_add = scaffold.add
 	api_edit = scaffold.edit
 	api_delete = scaffold.delete
+	admin_list = scaffold.list
+	admin_view = scaffold.view
+	admin_add = scaffold.add
+	admin_edit = scaffold.edit
+	admin_delete = scaffold.delete
